12f175fda9210c82cb2524190e8ff80a6beacc6b
Rafie Walker <slickytail.mc@gmail.com>
2020-11-24 13:17:20 -0500

    Put in the board

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 class CheckersGame():
 
     def __init__(self, board, to_move):
-        self.board = []
+        self.board = np.copy(board)
         self.game_over = False
         self.n_players = # number of unique values in board
         self.player_turn = to_move
@@ -13,9 +15,9 @@ class CheckersGame():
     @classmethod
     def new_game(cls, n_players):
         # Initialize the board to the default state
-        # ie, distribute the 
-        assert n_players % 2 == 0
-        board = []...
+        board = np.array(
+                [[BOARD_INT_MAP[c] for c in r] for r in BOARD_STR.splitlines()]
+            dtype=int)
         # cls is basically going to be CheckersGame
         # so calling CheckersGame.new_game(n) is going to call CheckersGame(board, 0) down here
         return cls(board, 0)
@@ -36,3 +38,28 @@ class CheckersGame():
     def get_legal(self):
         # Return a list of legal moves for the moving player.
         return []
+
+BOARD_STR = """\
+....4............
+....44...........
+....444..........
+....4444.........
+3333ooooo5555....
+.333oooooo555....
+..33ooooooo55....
+...3oooooooo5....
+....ooooooooo....
+....2oooooooo6...
+....22ooooooo66..
+....222oooooo666.
+....2222ooooo6666
+.........1111....
+..........111....
+...........11....
+............1...."""
+# I want to zero-index the players, but instead I'm going to make 0 be the empty space
+BOARD_INT_MAP = {
+    '.': -1, 'o': 0,
+    '1': 1, '2': 2, '3': 3,
+    '4': 4, '5': 5, '6': 6
+}
